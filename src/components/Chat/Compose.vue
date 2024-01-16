81caf5a3e22a370c0cef3909604ebf96190d320d
Tuurezki-Git <tuure.takala@gmail.com>
2024-01-16 20:46:45 +0200

    First test to environment. Not working

--- src/components/Chat/Compose.vue
+++ src/components/Chat/Compose.vue
@@ -23,6 +23,8 @@ const emit = defineEmits(["send"]);
 function send() {
   // Hint: Call 'emit' with the correct arguments.
   //       Finally, clear the text input.
+  this.$emit('send', this.text);
+  this.text = "";
 }
 </script>
 
@@ -32,10 +34,12 @@ function send() {
       <!-- Hint: See if you could send the message by pressing the enter key in the text field.
                  https://vuejs.org/guide/essentials/event-handling.html#key-modifiers
       -->
-      <Input :value="text" placeholder="Type a message" @change="onChange" />
+      <Input :value="text" placeholder="Type a message" @change="onChange" @keyup.enter="send" />
 
       <!-- Hint: Call the 'send()' function when the button emits a 'click' event. -->
-      <Button icon="send" />
+      <Button icon="send" 
+        @click="send" 
+      />
     </div>
   </div>
 </template>
